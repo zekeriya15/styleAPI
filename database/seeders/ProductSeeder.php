32b30468fbb6c938de0deddf6147b91ddf84896c
Muhamad Dzikriansyah <dzikriansyah1503@gmail.com>
2025-06-27 13:06:43 +0700

    create get operation

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
 
 class ProductSeeder extends Seeder
 {
@@ -12,6 +13,13 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Product::create([
+            'image' => 'test',
+            'name' => 'test name',
+            'size' => 'M',
+            'description' => 'test blah blah',
+            'price' => 89700.90,
+            'stock' => 78
+        ]);
     }
 }
